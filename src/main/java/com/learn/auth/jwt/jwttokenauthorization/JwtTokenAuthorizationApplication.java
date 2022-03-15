523fe9c6d7cb891114aac8f168e9ec64f05fb97e
Mostafijur Rahman <mrkajol550@gmail.com>
2022-03-15 12:51:18 +0600

    project sketch ready

---
+++ src/main/java/com/learn/auth/jwt/jwttokenauthorization/JwtTokenAuthorizationApplication.java
@@ -0,0 +1,14 @@
+package com.learn.auth.jwt.jwttokenauthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTokenAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtTokenAuthorizationApplication.class, args);
+    System.out.println("Hello Spring boot");
+	}
+
+}
---
+++ src/test/java/com/learn/auth/jwt/jwttokenauthorization/JwtTokenAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.auth.jwt.jwttokenauthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTokenAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
