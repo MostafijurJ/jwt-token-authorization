0a00e02d6296c4c45291813353d1b1bee202ad2b
MostafijurJ <mrkajol550@gmail.com>
2022-03-15 13:08:17 +0600

    JWT dependency added

---
+++ src/main/java/com/learn/auth/jwt/jwttokenauthorization/config/SecurityConfigurer.java
@@ -0,0 +1,8 @@
+package com.learn.auth.jwt.jwttokenauthorization.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+}
